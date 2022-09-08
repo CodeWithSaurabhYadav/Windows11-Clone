da2c84ddfc12b481a8d03a086d3536726e50e9ac
saurabh <50881320+CodeWithSaurabhYadav@users.noreply.github.com>
2022-09-08 16:43:30 +0530

    updating the previous changes commits

---
+++ app.py
@@ -0,0 +1,11 @@
+import os
+from random import randint as rd
+
+
+for i in range(1,150):
+    for j in range(0,rd(0,7)):
+        date = str(i) + 'days age'
+        with open("file.txt","a") as file:
+            file.write("test data\n")
+        os.system("git add .")
+        os.system('git commit --date="' + date + '" -m "updating the previous changes commits"')
